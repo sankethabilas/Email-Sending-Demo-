c1dab417fb03407931a815c9351a2c78bc72f7d5
Sanketh abilas <sankethmarasinghaop@gmail.com>
2024-07-27 13:30:42 +0530

    Basic UI of the main page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Email Sending Process</title>
+</head>
+
+<body>
+
+    <h1>Email Sending</h1>
+
+    <p>Recipient's Email</p>
+    <input type="email" />
+
+    <p>Subject</p>
+    <input type="text" />
+
+    <p>Content</p>
+    <textarea cols="30" rows="10"></textarea>
+    <br /><br />
+
+    <button>Send Email</button>
+
+</body>
+
+</html>
